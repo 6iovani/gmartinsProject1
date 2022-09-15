a0514aeebf52f4444d4c8d67019aed3d6eaee473
Giovani Martins <gio@Giovanis-MacBook-Pro.local>
2022-09-15 12:08:03 -0400

    Initial commit

---
+++ gmartinsProject1.py
@@ -0,0 +1,12 @@
+name = input("What is your name?")
+birthYear = input("What year were you born in?")
+birthYear = int(birthYear)
+currentYear = int("2022")
+age = currentYear-birthYear
+print("Hello, " + name + '!')
+answer = (f"You are: {age} years old.")
+print(answer)
+
+
+
+
